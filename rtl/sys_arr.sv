6f32a4704ab4671af7a2985bc3aa9a04facf95d4
Saul Rodriguez <saulargz35@gmail.com>
2025-05-09 07:27:03 -0700

    setup systolic array

--- rtl/sys_arr.sv
+++ rtl/sys_arr.sv
@@ -1,2 +1,32 @@
+`timescale 1ps/1ps
+
+module sys_arr(
+    input logic CLK,
+    input logic rst,
+    input logic [7:0] a,
+    input logic [7:0] b,
+    output logic [7:0] c
+);
+
+genvar i, j;
+generate
+    for (i = 0; i < M; i = i + 1 ) begin
+        for (j = 0; j < M ; j = j + 1 ) begin
+            mat_acc mat_acc(
+                .CLK(CLK),
+                .rst(rst),
+                .vld_in(vld_in),
+                .rdy_out(rdy_out),
+                .a(a),
+                .b(b),
+                .c(c),
+                .vld_out(vld_out),
+                .rdy_in(rdy_in)
+            )
+        end 
+    end
+endgenerate
+    
+endmodule
 
 
